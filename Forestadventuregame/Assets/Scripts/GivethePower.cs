ec15724939c042b69b9a3aef2f537028d097eca2
Brexton Lane <19.brexton.lane@ksd.org>
2018-10-09 10:27:06 -0700

    update

---
+++ Forestadventuregame/Assets/Scripts/GivethePower.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GivethePower : MonoBehaviour
+{
+  
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Shooting")
+        {
+            GetComponent<PlayerShoot>().enabled = true;
+            Destroy.gameObject(ThePower);
+        }
+    }
+}
