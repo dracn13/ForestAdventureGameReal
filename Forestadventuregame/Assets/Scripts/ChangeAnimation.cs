c5aca705b95261047205e05c8ec553d21e2d72cd
Brexton Lane <19.brexton.lane@ksd.org>
2018-10-15 09:55:35 -0700

    update

---
+++ Forestadventuregame/Assets/Scripts/ChangeAnimation.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeAnimation : MonoBehaviour
+
+{
+    public GameObject PlayerHat;
+    public GameObject PlayerHatless;
+    public RuntimeAnimatorController controller;
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "PlayerSwitch")
+        {
+            //PlayerHat.GetComponent<SpriteRenderer>().enabled = false;
+            //PlayerHatless.SetActive(true);
+            Animator a = GetComponent<Animator>();
+            float x = a.GetFloat("X");
+            float y = a.GetFloat("Y");
+            float Idle = a.GetFloat("Idle");
+
+            GetComponent<Animator>().runtimeAnimatorController = controller;
+        }
+
+    }
+}
