1f6cc495fec5753acc396e975a3f6e4719891c31
Brexton Lane <19.brexton.lane@ksd.org>
2018-10-16 09:49:12 -0700

    update

--- Forestadventuregame/Assets/Scripts/CutScene.cs
+++ Forestadventuregame/Assets/Scripts/CutScene.cs
@@ -9,20 +9,26 @@ public class CutScene : MonoBehaviour
 
     public GameObject Wall;
     public GameObject Text;
+    public GameObject Boss;
+    public GameObject Player;
     float timer = 0;
 
-    void OnTriggerStay2D(Collider2D collision)
+    void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "Player")
         {
             timer += Time.deltaTime;
             Wall.SetActive(true);
             Text.SetActive(true);
-            GetComponent<PlayerMovement>().enabled = false;
+            Player.GetComponent<PlayerMovement>().enabled = false;
+            Player.GetComponent<PlayerAnimationControls>().enabled = false;
+            Player.GetComponent<Rigidbody2D> ().velocity = new Vector2(0,0);
             if (timer > 1.0f)
             {
                 Text.SetActive(false);
-                GetComponent<PlayerMovement>().enabled = true;
+                Player.GetComponent<PlayerMovement>().enabled = true;
+                Player.GetComponent<PlayerAnimationControls>().enabled = false;
+                Boss.SetActive(false);
             }
         }
     }
