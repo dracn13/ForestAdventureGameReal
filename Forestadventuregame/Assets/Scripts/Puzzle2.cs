5c0159810a57ec85cd93d62172bce264cb498faf
Andrew Nelson <20.andrew.nelson@ksd.org>
2018-10-16 09:03:18 -0700

    Puzzle 2 start

--- Forestadventuregame/Assets/Scripts/Puzzle2.cs
+++ Forestadventuregame/Assets/Scripts/Puzzle2.cs
@@ -4,17 +4,41 @@ using UnityEngine;
 
 public class Puzzle2 : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
-     void OnTriggerEnter2D(Collider2D collision)
+    public GameObject Dark;
+    public int Buttons2 = 0;
+    public GameObject Door;
+
+
+    // Use this for initialization
+
+    void Start()
     {
-        
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player" || collision.gameObject.tag == "Enemy" && Dark.GetComponent<SpriteRenderer>().enabled == true)
+        {
+            Buttons2++;
+            collision.gameObject.GetComponent<ButtonCount2>().ModifyButtonCount2(1);
+            Dark.GetComponent<SpriteRenderer>().enabled = false;
+            Debug.Log(Buttons2);
+
+        }
+        else if (collision.gameObject.tag == "Player" || collision.gameObject.tag == "Enemy"  && Dark.GetComponent<SpriteRenderer>().enabled == false)
+        {
+            Dark.GetComponent<SpriteRenderer>().enabled = true;
+            Buttons2--;
+            collision.gameObject.GetComponent<ButtonCount2>().ModifyButtonCount2(-1);
+            Debug.Log("Door Count is" + Buttons2);
+        }
+
     }
 }
+
