8f67aad416f01b3a0572aeb3ed66177973ee76ef
Brexton Lane <19.brexton.lane@ksd.org>
2018-10-18 08:39:35 -0700

    update

---
+++ Forestadventuregame/Assets/Scripts/WitchAttack2.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WitchAttack2 : MonoBehaviour
+{
+    public GameObject player;
+    public float chaseTriggerDistance = 3.0f;
+    public GameObject prefab;
+    public float shootSpeed = 10;
+    float timer = 0;
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        Vector3 playerPosition = player.transform.position;
+        Vector3 chaseDirection = playerPosition - transform.position;
+        if (chaseDirection.magnitude < chaseTriggerDistance && timer > 1.0f)
+        {
+            timer = 0;
+            Vector3 shootDir = chaseDirection;
+            shootDir.Normalize();
+            Vector3 offset = shootDir;
+            shootDir = shootDir * shootSpeed;
+            //when calculating a vector from a to b
+            //always do destination - start position
+            GameObject Bullet = (GameObject)Instantiate(prefab, transform.position + offset, Quaternion.identity);
+            Bullet.GetComponent<Rigidbody2D>().velocity = shootDir;
+            Destroy(Bullet, 0.7f);
+        }
+    }
+}
